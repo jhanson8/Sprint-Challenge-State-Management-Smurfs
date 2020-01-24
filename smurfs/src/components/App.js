48ecf9c8404f2a80c484f9c6338a963ada0dbecc
Jonathan Hanson <jhanson8@uoregon.edu>
2020-01-24 09:15:42 -0800

    add form comp

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
 import Smurf from './Smurf.js';
+import NewPost from './NewSmurf.js';
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
       <Smurf />
+      <NewPost/>
       </div>
     );
   }
